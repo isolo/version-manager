79bc0b9debdb7804fd3964e93690e7913181e812
isolo <ilya.i.solo@gmail.com>
2016-09-22 17:57:13 +0300

    Added basic git integration

---
+++ lib/version-manager/vcs/git.rb
@@ -0,0 +1,40 @@
+module VersionManager
+  module VCS
+    class Git
+      def initialize
+        @git = ::Git.open(ROOT_DIR)
+      end
+
+      def current_branch
+        git.branch.name
+      end
+
+      def master_state_actual?
+        git.revparse(master_branch_name) == git.revparse(remote_master_branch_name)
+      end
+
+      def state_actual?
+        head = ::Git.ls_remote['head']
+        remote_head = find_remote_branch('HEAD').last
+        return unless remote_head
+        head[:sha] == remote_head[:sha]
+      end
+
+      private
+
+      attr_reader :git
+
+      def master_branch_name
+        'master'
+      end
+
+      def remote_master_branch_name
+        find_remote_branch(master_branch_name).first
+      end
+
+      def find_remote_branch(branch_name)
+        ::Git.ls_remote['remotes'].find { |remote, _| branch_name == remote.split('/').last }
+      end
+    end
+  end
+end
