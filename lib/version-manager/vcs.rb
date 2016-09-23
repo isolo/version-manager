38e7a5f0b860fc4da3028df1542c98a40b650ddc
isolo <ilya.i.solo@gmail.com>
2016-09-23 16:17:03 +0300

    Added some exception classes to VCS

---
+++ lib/version-manager/vcs.rb
@@ -0,0 +1,30 @@
+module VersionManager
+  module VCS
+    class BranchAlreadyExistsError
+      def initialize(branch_name)
+        @branch_name = branch_name
+      end
+
+      def message
+        "Branch #{@branch_name} already exists"
+      end
+    end
+
+    class UnsupportedVCSError
+      def initialize(vcs)
+        @vcs = vcs
+      end
+
+      def message
+        "VCS #{vcs} has not been supported yet"
+      end
+    end
+
+    def self.build
+      case VersionManager.options[:vcs][:name]
+      when 'git' then VersionManager::VCS::Git.new
+      else raise UnsupportedVCSError
+      end
+    end
+  end
+end
