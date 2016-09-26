590fcaed6ec9dd48fe943ed6145822ce966e43c0
isolo <ilya.i.solo@gmail.com>
2016-09-26 13:25:28 +0300

    Changed implementation of latest_version

--- lib/version-manager/version_storage.rb
+++ lib/version-manager/version_storage.rb
@@ -17,7 +17,10 @@ module VersionManager
       versions = vcs.remote_branch_names.map do |name|
         ReleaseVersion.new(name) if ReleaseVersion.valid?(name)
       end
-      versions.compact.sort.last
+      version = versions.compact.sort.last
+      file_content = vcs.show_file(branch_name(version), full_path) if version
+      version = ReleaseVersion.new(file_content) if file_content && ReleaseVersion.valid?(file_content)
+      version
     end
 
     private
@@ -26,6 +29,10 @@ module VersionManager
 
     def full_path
       File.expand_path(File.join(filepath, filename))
+    end
+
+    def branch_name(version)
+      "release-#{version.to_s}"
     end
   end
 end
