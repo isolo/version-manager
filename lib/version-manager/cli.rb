7b518cc83a7690b9028d62019c456cc4470a27b1
isolo <ilya.i.solo@gmail.com>
2016-09-26 13:22:37 +0300

    Added handlers to cli

--- lib/version-manager/cli.rb
+++ lib/version-manager/cli.rb
@@ -1,37 +1,59 @@
-class VersionManager::CLI
-  def initialize(exec_name: __FILE__)
-    @exec_name = exec_name
-  end
-
-  def start
-    doc = <<~DOCOPT
-
-    Usage:
-      #{exec_name} make release
-      #{exec_name} make hotfix
-      #{exec_name} -h | --help
-      #{exec_name} -v | --version
-
-    Options:
-      -h --help     show this screen.
-      -v --version  show version.
-    DOCOPT
-
-    begin
-      require 'byebug'
-      parse_options(Docopt::docopt(doc))
-    rescue Docopt::Exit => e
-      puts e.message
+module VersionManager
+  class CLI
+    def initialize(exec_name: __FILE__)
+      @exec_name = exec_name
     end
-  end
 
-  private
+    def start
+      doc = <<~DOCOPT
 
-  attr_reader :exec_name
+      Usage:
+        #{exec_name} make major
+        #{exec_name} make minor
+        #{exec_name} make patch
+        #{exec_name} -h | --help
+        #{exec_name} -v | --version
 
-  def parse_options(options)
-    if options['--version']
-      puts VersionManager::VERSION
+      Options:
+        -h --help     show this screen.
+        -v --version  show version.
+      DOCOPT
+
+      begin
+        parse_options(Docopt::docopt(doc))
+      rescue Docopt::Exit => e
+        puts e.message
+      end
+    end
+
+    private
+
+    attr_reader :exec_name
+
+    def parse_options(options)
+      if options['--version']
+        puts VersionManager::VERSION
+      end
+      %w(major minor patch).each do |release|
+        next unless options.keys.include?(release)
+        break make_release(release.to_sym)
+      end
+    end
+
+    def make_release(release_type)
+      storage_options = VersionManager.options[:storage]
+      storage = VersionStorage.new(VCS.build, storage_options)
+      version = storage.latest_version
+      set_initial_version unless version
+      Make.new(version, VCS.build, storage).public_send("#{release_type}!")
+    end
+
+    def set_initial_version
+      puts 'There are no any versions. Please, input an initial one:'
+      ReleaseVersion.new(STDIN.gets)
+    rescue ArgumentError => e
+      puts e.message
+      retry
     end
   end
 end
