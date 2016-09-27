3e8312dbf15d2a2c61ff468340e740186b38c14b
isolo <ilya.i.solo@gmail.com>
2016-09-27 17:28:06 +0300

    Added bumping tag to bump_patch

--- lib/version-manager/make.rb
+++ lib/version-manager/make.rb
@@ -35,6 +35,8 @@ module VersionManager
       raise ForbiddenBranchError unless appropriate_branch_for?('patch')
       version.bump_patch
       vcs.commit(version_storage.store(version), default_commit_message)
+      vcs.add_tag(version.to_s, default_commit_message)
+      vcs.push_tag(version.to_s)
       vcs.push
     end
 
